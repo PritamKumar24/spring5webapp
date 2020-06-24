7713ce36e18ef13c916b71074747e98aacc23d51
pritamkumar24 <pritamdabby@gmail.com>
2020-06-24 17:14:53 -0400

    MVC Pattern with thymeleaf

--- src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -1,4 +1,70 @@
 package guru.springframework.spring5webapp.bootstrap;
 
-public class BootStrapData {
+import guru.springframework.spring5webapp.domain.Author;
+import guru.springframework.spring5webapp.domain.Book;
+import guru.springframework.spring5webapp.domain.Publisher;
+import guru.springframework.spring5webapp.repositories.AuthorRepository;
+import guru.springframework.spring5webapp.repositories.BooKRepository;
+import guru.springframework.spring5webapp.repositories.PublisherRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import javax.swing.*;
+
+@Component
+public class BootStrapData implements CommandLineRunner {
+
+    private final AuthorRepository authorRepository;
+    private final BooKRepository booKRepository;
+    private final PublisherRepository publisherRepository;
+
+    public BootStrapData(AuthorRepository authorRepository, BooKRepository booKRepository, PublisherRepository publisherRepository) {
+        this.authorRepository = authorRepository;
+        this.booKRepository = booKRepository;
+        this.publisherRepository = publisherRepository;
+    }
+
+    //alt+Insert
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Started in Bootstrap");
+
+        Publisher publisher = new Publisher();
+        publisher.setName("SFG Publishing");
+        publisher.setCity("St Peterburg");
+        publisher.setState("FL");
+
+        publisherRepository.save(publisher);
+
+        System.out.println("Publisher Count: " + publisherRepository.count());
+
+        Author eric = new Author("Eric", "Evans");
+        Book ddd = new Book("Domain Driven Design",  "123123");
+        eric.getBooks().add(ddd);
+        ddd.getAuthors().add(eric);
+
+        ddd.setPublisher(publisher);
+        publisher.getBooks().add(ddd);
+
+        authorRepository.save(eric);
+        booKRepository.save(ddd);
+        publisherRepository.save(publisher);
+
+        Author rod = new Author("Rod", "Johnson");
+        Book noEJB = new Book("J2EE Development without EJB",  "39779797");
+
+        noEJB.setPublisher(publisher);
+        publisher.getBooks().add(noEJB);
+
+        rod.getBooks().add(noEJB);
+        noEJB.getAuthors().add(rod);
+
+        authorRepository.save(rod);
+        booKRepository.save(noEJB);
+        publisherRepository.save(publisher);
+
+        System.out.println("Number of books: " + booKRepository.count());
+        System.out.println("Publisher Number of Books:" + publisher.getBooks().size());
+
+    }
 }
--- src/main/java/guru/springframework/spring5webapp/controllers/BookController.java
+++ src/main/java/guru/springframework/spring5webapp/controllers/BookController.java
@@ -1,4 +1,24 @@
 package guru.springframework.spring5webapp.controllers;
 
+import guru.springframework.spring5webapp.repositories.BooKRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class BookController {
+
+    private BooKRepository booKRepository;
+
+    public BookController(BooKRepository booKRepository) {
+        this.booKRepository = booKRepository;
+    }
+
+    @RequestMapping("/books")
+    public String getBooks(Model model){
+
+        model.addAttribute("books", booKRepository.findAll());
+
+        return "books/list";
+    }
 }
